c559a1c714d6dc2f9a044f6f7867a4a19cd47ce3
James Scanlon <jscanlon77@outlook.com>
2019-02-18 16:11:49 +0000

    WE don't want all the obj/bin stuff

--- CalculationsConsole/bin/Debug/libraries/xirr/xirrcalc.r
+++
@@ -1,8 +0,0 @@
-xirr <- function(cf, t) {
-    npv <- function(cf, t, r) sum(cf / ((r) ^ (t / 365.25)))
-    tryCatch(
-      irr <- uniroot(f = npv, interval = c(0, 100), cf = cf, t = t, maxiter = 100)$root - 1,
-      error = return(0)
-    )
-    return(irr)
-}
